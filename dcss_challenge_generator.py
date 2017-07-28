baff2a890654ff92dc38f5e994e8645cfa9431ac
Kiel97 <kkielek97@gmail.com>
2017-07-28 23:22:42 +0200

    Added import no combo function
    Renamed also import_database to import_to_dictionary

--- dcss_challenge_generator.py
+++ dcss_challenge_generator.py
@@ -3,7 +3,7 @@
 from random import choice
 import sys
 
-def import_database(filename):
+def import_to_dictionary(filename):
     """Imports database from file to dictionary"""
     print("Importing %s..." % filename[:-12],end="")
     
@@ -33,12 +33,43 @@ def import_database(filename):
     
     return dictionary
 
+def import_non_available_combos(filename):
+    """Imports bad combos database from file to list"""
+    print("Importing non available combos...",end="")
+
+    try:
+        file = open(filename,"r")
+    except FileNotFoundError:
+        print("Failure!\nFile not found! Check if name of file is %s" % filename)
+        input("Press any key to exit.")
+        sys.exit(1)
+
+    imp_list = []
+    iteration = 0
+    
+    for line in file:
+        iteration += 1
+        line = line.strip()
+
+        if len(line) != 4:
+            print("Failure!\n%s (line %d) is not 4-char! Correct it in %s file!" % (line,iteration,filename))
+            input("Press any key to exit.")
+            sys.exit(2)
+            
+        imp_list.append(line)
+
+    return imp_list
+
 def main():
     """This is where program starts"""
 
-    species_database = import_database("species_database.txt")
-    backgrounds_database = import_database("background_database.txt")
- 
+    species_database = import_to_dictionary("species_database.txt")
+    backgrounds_database = import_to_dictionary("background_database.txt")
+
+    no_combo_database = import_non_available_combos("nocombo_database.txt")
+
+    print(no_combo_database)
+    
     print("Thank you for using this software!")
     input("Press any key to exit.")
 
